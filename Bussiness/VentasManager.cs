238a50763bfe394bd0ef9aabdfb61b78e9c5e123
QbicTeam <carlossotoocio@hotmail.com>
2021-01-26 18:55:33 -0800

    VentasManager Codigo Parcial

--- Bussiness/VentasManager.cs
+++ Bussiness/VentasManager.cs
@@ -14,7 +14,7 @@ namespace BlackBox.Bussiness
     {
         private SqlConnection _conn;
 
-        /* private const string FLD_VENTAID = "ventaId";
+        private const string FLD_VENTAID = "ventaId";
         private const string FLD_FECHAHORA = "fechaHora";
         private const string FLD_CAJERO = "cajero";
         private const string FLD_RECIBO = "recibo";
@@ -23,15 +23,43 @@ namespace BlackBox.Bussiness
         private const string FLD_IMPUESTOS = "Impuestos";
         private const string FLD_TOTAL = "total";
         private const string FLD_EN_CORTE = "enCorte";
-        private const string FLD_CANCELADO = "cancelado";*/
-        private const string FLD_VTA = "vta";
-        private const string FLD_VTA_DT = "vtaDT";
+        private const string FLD_CANCELADO = "cancelado";
+        //--
+        private const string FLD_ID = "Id";
+        private const string FLD_PRODUCTO = "Producto";
+        private const string FLD_CANTIDAD = "Cantidad";
+        private const string FLD_PRECIO = "Precio";
+        private const string FLD_NIVEL = "Nivel";
+        private const string FLD_PRODUCTO_PADRE = "ProductoPadre";
+        //--
+        private const string FLD_NOPRODUCTOS = "NoProductos"; 
+        private const string FLD_VENTA = "Venta";
+        private const string FLD_VENTA_TOTAL = "VentaTotal";
 
+        private const string PRM_VTA = "vta";
+        private const string PRM_VTA_DT = "vtaDT";
+        private const string PRM_NORECIBO = "noRecibo";
+        private const string PRM_VTAID = "vtaId";
+        private const string PRM_CAJERO = "cajero";
 
         private const string SP_SAVE_VENTAS = "ventas_save";
         private const string SP_UPDATE_VENTA = "ventas_update";
         private const string SP_SAVE_VENTASDT = "ventasDT_save";
 
+        private const string SP_GET_TICKET = "getTicket";
+        private const string SP_CANCEL_TICKET = "cancelTicket";
+        private const string SP_GET_VENTAS = "getVentas";
+        private const string SP_GET_VENTAS_CAJERO = "getVentasCajero";
+        private const string SP_GET_VENTAS_SIN_CORTE = "getVentasSinCorte";
+        private const string SP_GET_CORTEZ = "getCorteZ";
+
+        /*
+        -- getTicket](@noRecibo varchar(15))
+ -- cancelTicket](@vtaId int)
+ -- getVentas]
+ -- getVentasCajero] (@cajero varchar(100))
+ -- getVentasSinCorte]
+ -- getCorteZ]*/
 
         public VentasManager(string connectionString)
         {
@@ -41,10 +69,8 @@ namespace BlackBox.Bussiness
         public int SaveVenta(Venta vta)
         {
             
-
             SqlCommand cmd = new SqlCommand(SP_SAVE_VENTAS, this._conn);
             cmd.CommandType = CommandType.StoredProcedure;
-
 
             string xml;
 
@@ -56,20 +82,17 @@ namespace BlackBox.Bussiness
             }
 
             SqlParameter prm = new SqlParameter();
-            prm.ParameterName = FLD_VTA;
+            prm.ParameterName = PRM_VTA;
             prm.Direction = ParameterDirection.Input;
             prm.SqlDbType = SqlDbType.Xml;
             //prm.Size = 100;
             prm.Value = xml;
             cmd.Parameters.Add(prm);
 
-            
-
             this._conn.Open();
 
             int id = Convert.ToInt32(cmd.ExecuteScalar());
 
-            
             this._conn.Close();
 
             return id;
@@ -77,10 +100,8 @@ namespace BlackBox.Bussiness
         public bool SaveVentaDT(Venta vta)
         {
 
-
             SqlCommand cmd = new SqlCommand(SP_SAVE_VENTASDT, this._conn);
             cmd.CommandType = CommandType.StoredProcedure;
-
 
             string xml;
 
@@ -92,23 +113,213 @@ namespace BlackBox.Bussiness
             }
 
             SqlParameter prm = new SqlParameter();
-            prm.ParameterName = FLD_VTA_DT;
+            prm.ParameterName = PRM_VTA_DT;
             prm.Direction = ParameterDirection.Input;
-            prm.SqlDbType = SqlDbType.Xml;
-            //prm.Size = 100;
+            prm.SqlDbType = SqlDbType.Xml;            
             prm.Value = xml;
             cmd.Parameters.Add(prm);
-
-
 
             this._conn.Open();
 
             int id = Convert.ToInt32(cmd.ExecuteScalar());
 
-
             this._conn.Close();
 
             return id > 0 ? true : false;
+        }
+
+        public Venta GetTicket(string noRecibo)
+        {
+            var vtas = new List<Venta>();
+            var vta = new Venta();
+            
+            SqlCommand cmd = new SqlCommand(SP_GET_TICKET, this._conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            SqlParameter prm = new SqlParameter();
+            prm.ParameterName = PRM_NORECIBO;
+            prm.Direction = ParameterDirection.Input;
+            prm.SqlDbType = SqlDbType.VarChar;
+            prm.Size = 15;
+            prm.Value = noRecibo;
+            cmd.Parameters.Add(prm);
+
+            this._conn.Open();
+
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            vtas = FillVentas(reader);
+
+            if (vtas.Count > 0)
+                vta = vtas[0];
+
+            this._conn.Close();
+
+            return vta;
+        }
+
+        private List<Venta> FillVentas(SqlDataReader reader)
+        {
+            var vtas = new List<Venta>();
+            var vta = new Venta();
+            var dt = new VentaDT();
+
+            var vtaId = 0;
+            if (reader.Read())
+            {
+                if (vtaId != Convert.ToInt32(reader[FLD_VENTAID]))
+                {
+                    vta = new Venta()
+                    {
+                        VentaId = Convert.ToInt32(reader[FLD_VENTAID]),
+                        FechaHora = Convert.ToDateTime(reader[FLD_FECHAHORA]),
+                        Cajero = reader[FLD_CAJERO].ToString(),
+                        Recibo = reader[FLD_RECIBO].ToString(),
+                        CantidadArticulos = Convert.ToInt32(reader[FLD_CANTIDAD_ARTICULOS]),
+                        Total = Convert.ToDecimal(reader[FLD_TOTAL]),
+                        SubTotal = Convert.ToDecimal(reader[FLD_SUBTOTAL]),
+                        EnCorte = Convert.ToInt32(reader[FLD_EN_CORTE]),
+                        Cancelado = Convert.ToBoolean(reader[FLD_CANCELADO])
+                    };
+                    vta.SubTotal = vta.Total - vta.Impuestos;
+                    vta.Productos = new List<VentaDT>();
+
+                    vtas.Add(vta);
+                }
+
+                dt = new VentaDT()
+                {
+                    Id = Convert.ToInt32(reader[FLD_ID]),
+                    VentaId = Convert.ToInt32(reader[FLD_VENTAID]),
+                    Producto = reader[FLD_PRODUCTO].ToString(),
+                    Cantidad = Convert.ToInt32(reader[FLD_CANTIDAD]),
+                    Precio = Convert.ToDecimal(reader[FLD_PRECIO]),
+                    Nivel = Convert.ToInt32(reader[FLD_NIVEL]),
+                    ProductoPadre = reader[FLD_PRODUCTO_PADRE].ToString()
+                };
+
+                vta.Productos.Add(dt);
+            }
+
+            return vtas;
+        }
+        public bool CancelTicket(int vtaId)
+        {
+            var result = true;
+ 
+            SqlCommand cmd = new SqlCommand(SP_CANCEL_TICKET, this._conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            SqlParameter prm = new SqlParameter();
+            prm.ParameterName = PRM_VTAID;
+            prm.Direction = ParameterDirection.Input;
+            prm.SqlDbType = SqlDbType.Int;
+            prm.Value = vtaId;
+            cmd.Parameters.Add(prm);
+
+            this._conn.Open();
+
+            cmd.ExecuteNonQuery();
+
+            this._conn.Close();
+
+            return result;
+        }
+
+        public List<Venta> GetVentas()
+        {
+            var vtas = new List<Venta>();
+
+            SqlCommand cmd = new SqlCommand(SP_GET_VENTAS, this._conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            this._conn.Open();
+
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            vtas = FillVentas(reader);
+
+            this._conn.Close();
+            
+            return vtas;
+        }
+        public List<Venta> GetVentasCajero(string cajero)
+        {
+            var vtas = new List<Venta>();
+
+            SqlCommand cmd = new SqlCommand(SP_GET_VENTAS_CAJERO, this._conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            SqlParameter prm = new SqlParameter();
+            prm.ParameterName = PRM_CAJERO;
+            prm.Direction = ParameterDirection.Input;
+            prm.SqlDbType = SqlDbType.VarChar;
+            prm.Size = 100;
+            prm.Value = cajero;
+            cmd.Parameters.Add(prm);
+
+            this._conn.Open();
+
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            vtas = FillVentas(reader);
+
+            this._conn.Close();
+
+            return vtas;
+        }
+        public List<Venta> GetVentasSinCorte()
+        {
+            var vtas = new List<Venta>();
+
+            SqlCommand cmd = new SqlCommand(SP_GET_VENTAS_SIN_CORTE, this._conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            this._conn.Open();
+
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            vtas = FillVentas(reader);
+
+            this._conn.Close();
+
+            return vtas;
+        }
+        public CorteZ GetCorteZ()
+        {
+            var cortez = new CorteZ();
+            var prodz = new ProductoZ();
+            cortez.Productos = new List<ProductoZ>();
+
+            SqlCommand cmd = new SqlCommand(SP_GET_CORTEZ, this._conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            this._conn.Open();
+
+            SqlDataReader reader = cmd.ExecuteReader();
+            
+            if (reader.Read())
+            {
+                prodz = new ProductoZ()
+                { 
+                    Producto = reader[FLD_PRODUCTO].ToString(),
+                    NoProductos = Convert.ToInt32(reader[FLD_NOPRODUCTOS]),
+                    Venta = Convert.ToDecimal(reader[FLD_VENTA])
+                };
+
+                cortez.Productos.Add(prodz);
+            }
+
+            reader.NextResult();
+
+            if (reader.Read())
+            {
+                cortez.VentaTotal = Convert.ToDecimal(reader[FLD_VENTA_TOTAL]);
+            }
+
+            this._conn.Close();
+
+            return cortez;
         }
 
     }
---
+++ Model/CorteZ.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackBox.Model
+{
+    public class CorteZ
+    {
+        public string Sucursal { get; set; }
+        public string CodSucursal { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Supervisor { get; set; }
+        public decimal VentaTotal { get; set; }
+        public List<ProductoZ> Productos { get; set; }
+    }
+}
